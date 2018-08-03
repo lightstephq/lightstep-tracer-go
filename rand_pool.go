6aefe5cb68c69ba4906bb3d89efc85a12d95a579
Zuwei Li <li.zuwei@outlook.com>
2018-08-03 11:45:36 +0800

    add random pool to reduce concurrency on genSeededGUID()

---
+++ rand_pool.go
@@ -0,0 +1,60 @@
+package lightstep
+
+import (
+	"math/rand"
+	"time"
+	"sync/atomic"
+)
+
+// RandPool represents a pool of random number generators.
+// To generate a random id, round robin through the source pool with atomic increment.
+// With more and more goroutines, RandPool improves the performance of Random vs naive global random
+// mutex exponentially.
+// Try tests with 20000 goroutines and 500 calls to observe the difference
+type RandPool struct {
+	sources []*LockedRand
+	counter uint64 // used for round robin
+	size    uint64
+}
+
+// see bit twiddling hacks: https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+func nextNearestPow2uint64(v uint64) uint64 {
+	v--
+	v |= v >> 1
+	v |= v >> 2
+	v |= v >> 4
+	v |= v >> 8
+	v |= v >> 16
+	v |= v >> 32
+	v++
+	return v
+}
+
+// NewRandPool takes in a size and creates a pool of random id generators with size equal to next closest power of 2.
+// eg: NewPool(10) returns a pool with 2^4 = 16 random sources.
+func NewRandPool(seed int64, size uint64) *RandPool {
+	groupsize := nextNearestPow2uint64(size)
+	pool := &RandPool{
+		size:    groupsize,
+		sources: make([]*LockedRand, groupsize),
+	}
+	// seed the pool
+	pool.seed(seed)
+	return pool
+}
+
+// seed initializes the pool using a randomized sequence with given seed.
+func (r *RandPool) seed(seed int64) {
+	// init a random sequence to seed all sources
+	seedRan := rand.NewSource(time.Now().UnixNano())
+	for i := uint64(0); i < r.size; i++ {
+		r.sources[i] = NewLockedRand(seedRan.Int63())
+	}
+}
+
+// Pick returns a LockedRand from a pool of LockedRand
+func (r *RandPool) Pick() *LockedRand {
+	// use round robin with fast modulus of pow2 numbers
+	selection := atomic.AddUint64(&r.counter, 1) & (r.size - 1)
+	return r.sources[selection]
+}
--- util.go
+++ util.go
@@ -1,36 +1,20 @@
 package lightstep
 
 import (
-	"math/rand"
-	"sync"
+	"runtime"
 	"time"
 )
 
 var (
-	seededGUIDGen     *rand.Rand
-	seededGUIDGenOnce sync.Once
-	seededGUIDLock    sync.Mutex
+	// create a random pool with size equal to number of CPU Cores
+	randompool = NewRandPool(time.Now().UnixNano(), uint64(runtime.NumCPU()))
 )
 
 func genSeededGUID() uint64 {
-	// Golang does not seed the rng for us. Make sure it happens.
-	seededGUIDGenOnce.Do(func() {
-		seededGUIDGen = rand.New(rand.NewSource(time.Now().UnixNano()))
-	})
-
-	// The golang random generators are *not* intrinsically thread-safe.
-	seededGUIDLock.Lock()
-	defer seededGUIDLock.Unlock()
-	return uint64(seededGUIDGen.Int63())
+	return uint64(randompool.Pick().Int63())
 }
 
 func genSeededGUID2() (uint64, uint64) {
-	// Golang does not seed the rng for us. Make sure it happens.
-	seededGUIDGenOnce.Do(func() {
-		seededGUIDGen = rand.New(rand.NewSource(time.Now().UnixNano()))
-	})
-
-	seededGUIDLock.Lock()
-	defer seededGUIDLock.Unlock()
-	return uint64(seededGUIDGen.Int63()), uint64(seededGUIDGen.Int63())
+	n1, n2 := randompool.Pick().TwoInt63()
+	return uint64(n1), uint64(n2)
 }
