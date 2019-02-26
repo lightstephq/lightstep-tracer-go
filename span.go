706963fdf1cf9b4494c33267b80d442c500433f6
Isobel Redelmeier <iredelmeier@gmail.com>
2019-02-26 16:11:58 -0500

    Refactor function IsMetaSpan to method Span#IsMeta

--- span.go
+++ span.go
@@ -78,7 +78,7 @@ ReferencesLoop:
 	sp.raw.Duration = -1
 	sp.raw.Tags = opts.Options.Tags
 
-	if tracer.opts.MetaEventReportingEnabled && IsMetaSpan(sp) {
+	if tracer.opts.MetaEventReportingEnabled && sp.IsMeta() {
 		ot.StartSpan(LSMetaEvent_SpanStartOperation,
 			ot.Tag{Key: LSMetaEvent_MetaEventKey, Value: true},
 			ot.Tag{Key: LSMetaEvent_SpanIdKey, Value: sp.raw.Context.SpanID},
@@ -241,7 +241,7 @@ func (s *spanImpl) FinishWithOptions(opts ot.FinishOptions) {
 	s.raw.Duration = duration
 
 	s.tracer.RecordSpan(s.raw)
-	if s.tracer.opts.MetaEventReportingEnabled && IsMetaSpan(s) {
+	if s.tracer.opts.MetaEventReportingEnabled && s.IsMeta() {
 		ot.StartSpan(LSMetaEvent_SpanFinishOperation,
 			ot.Tag{Key: LSMetaEvent_MetaEventKey, Value: true},
 			ot.Tag{Key: LSMetaEvent_SpanIdKey, Value: s.raw.Context.SpanID},
@@ -278,4 +278,8 @@ func (s *spanImpl) Operation() string {
 
 func (s *spanImpl) Start() time.Time {
 	return s.raw.Start
+}
+
+func (s *spanImpl) IsMeta() bool {
+	return s.raw.Tags["lightstep.meta_event"] != nil
 }
